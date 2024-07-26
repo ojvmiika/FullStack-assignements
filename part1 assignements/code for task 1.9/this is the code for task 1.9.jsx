99c41fe3d10a0dfa6f11a5fa93ae555e2f4fc6b5
ojvmiika <165137497+ojvmiika@users.noreply.github.com>
2024-07-26 14:31:15 +0300

    Update this is the code for task 1.9.jsx

--- part1 assignements/code for task 1.9/this is the code for task 1.9.jsx
+++ part1 assignements/code for task 1.9/this is the code for task 1.9.jsx
@@ -1,1 +1,64 @@
+import { useState } from 'react'
 
+const Statistics = (props) => {
+  if (props.all ===0){
+    return( 
+    <p>No feedback given</p>  
+    )
+  }
+  else
+  return (
+    <div>
+  <h2>statics</h2>
+    <p>good {props.good}</p>
+    <p>neutral {props.neutral}</p>
+    <p>bad {props.bad}</p>
+    <p>all {props.all}</p>
+    <p>average {props.average}</p>
+    <p>positive {props.positive} %</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [total, setAverage] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+    setAverage(total +1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+    setAverage(total -1)
+  }
+
+  const average = (total / all)
+  const positive = (good / all)*100
+  
+
+  return (
+    <div>
+    <h1>give feedback</h1>
+    <button onClick = {handleGoodClick}>Good</button>
+    <button onClick = {handleNeutralClick}>Neutral</button>
+    <button onClick = {handleBadClick}>Bad</button>
+    <Statistics good = {good} neutral = {neutral} bad = {bad} all = {all} average = {average} positive ={positive}
+    />
+    </div>
+  )
+}
+
+export default App
