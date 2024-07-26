ccd056c74849ed2c9e9d0bd6fec8e496622ca995
ojvmiika <165137497+ojvmiika@users.noreply.github.com>
2024-07-26 10:26:50 +0300

    Update this is the code for task 1.2.jsx

--- part1 assignements/code for task 1.2/this is the code for task 1.2.jsx
+++ part1 assignements/code for task 1.2/this is the code for task 1.2.jsx
@@ -33,7 +33,7 @@ const App = () => {
   const Total = (props) => {
     return(
       <div>
-        <p>Number of exercises {props[0].exercises + props[1].exercises + props[2].exercises}</p>
+        <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
         </div>
     )
   }
